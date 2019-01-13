eb390ddfdc45288e675384d762ea4a7fb648e8b3
SumitSh7 <sumitsh2012@gmail.com>
2019-01-13 17:22:07 +0530

    Unity Begins

---
+++ Unity Basic 1/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string Name; // Name of our player
+
+    /*All the following define our charecteristics*/
+    public float Attackspeed; // Float for attack speed
+    public int Attack; //Integer for Attack Power
+    public int health; // Integer for our health
+    
+    /*All the following define Player World values*/
+    public float Gravity = 10; 
+    public float Speed = 10;
+    public float max = 10;
+
+    private readonly bool dead; // True or False for alive or dead
+    private Transform PlayerTransform;
+    private GameObject Enemy;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
